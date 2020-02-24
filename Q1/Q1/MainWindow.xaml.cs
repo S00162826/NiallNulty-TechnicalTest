013ca5c1d07b33f4ae92c73669f37bb15783aeeb
S00162826 <S00162826@mail.itsligo.ie>
2020-02-24 15:23:59 +0000

    LOC Segments parsed and "+" removed from lists

--- Q1/Q1/MainWindow.xaml.cs
+++ Q1/Q1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Q1
 {
@@ -23,6 +24,29 @@ namespace Q1
         public MainWindow()
         {
             InitializeComponent();
+
+            //Loading in text file
+            StreamReader sr = new StreamReader("Q1.txt");
+
+            //creating a list if each line in txt document
+            List<string> lines = new List<string>();
+            while (!sr.EndOfStream)
+                lines.Add(sr.ReadLine());
+            sr.Close();
+
+            //populating textbox only with required lines
+            txtBlocLOC.Text = lines[4] + "\n"
+                + lines[5] + "\n"
+                + lines[6] + "\n"
+                + lines[7] + "\n"
+                + lines[8];
+
+            //Replacing all "+" with an empty space
+            txtBlocSegments.Text = lines[4].Substring(4).Replace("+", " ").Remove(lines[4].Length -1,1) + "\n"
+                + lines[5].Substring(4).Replace("+", " ") + "\n"
+                + lines[6].Substring(4).Replace("+", " ") + "\n"
+                + lines[7].Substring(4).Replace("+", " ") + "\n"
+                + lines[8].Substring(4).Replace("+", " ");
         }
     }
 }
