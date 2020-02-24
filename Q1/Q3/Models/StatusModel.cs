12efb86fdfa78781ef1d81ac0f9de9ff06daefde
S00162826 <S00162826@mail.itsligo.ie>
2020-02-24 15:59:48 +0000

    Q started
    Created MVC app, Created model class

---
+++ Q1/Q3/Models/StatusModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Q3.Models
+{
+    public class StatusModel
+    {
+        public int statusResultA { get; set; }
+        public int statusResultB { get; set; }
+        public int statusResultC { get; set; }
+    }
+}
