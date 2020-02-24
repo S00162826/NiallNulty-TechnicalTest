867ddb6668da9a7fad9d31d5316b771b72d35090
S00162826 <S00162826@mail.itsligo.ie>
2020-02-24 15:45:57 +0000

    Q1 complete

--- Q1/Q1/MainWindow.xaml.cs
+++ Q1/Q1/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Q1
 {
@@ -28,11 +29,11 @@ namespace Q1
             //Loading in text file
             StreamReader sr = new StreamReader("Q1.txt");
 
-            //creating a list if each line in txt document
+            //creating a list of each line in txt document
             List<string> lines = new List<string>();
             while (!sr.EndOfStream)
                 lines.Add(sr.ReadLine());
-            sr.Close();
+             sr.Close();
 
             //populating textbox only with required lines
             txtBlocLOC.Text = lines[4] + "\n"
@@ -41,12 +42,26 @@ namespace Q1
                 + lines[7] + "\n"
                 + lines[8];
 
+            //Replacing all "'" with an empty space
+            lines[4] = lines[4].Replace("'", " ");
+            lines[5] = lines[5].Replace("'", " ");
+            lines[6] = lines[6].Replace("'", " ");
+            lines[7] = lines[7].Replace("'", " ");
+            lines[8] = lines[8].Replace("'", " ");
+
             //Replacing all "+" with an empty space
-            txtBlocSegments.Text = lines[4].Substring(4).Replace("+", " ").Remove(lines[4].Length -1,1) + "\n"
-                + lines[5].Substring(4).Replace("+", " ") + "\n"
-                + lines[6].Substring(4).Replace("+", " ") + "\n"
-                + lines[7].Substring(4).Replace("+", " ") + "\n"
-                + lines[8].Substring(4).Replace("+", " ");
+            txtBlocSegments.Text = lines[4].Replace("+", " ") + "\n"
+                + lines[5].Replace("+", " ") + "\n"
+                + lines[6].Replace("+", " ") + "\n"
+                + lines[7].Replace("+", " ") + "\n"
+                + lines[8].Replace("+", " ");
+
+
+            //alternatively, could write a method that finds all lines containing "LOC" and display them
+
+            //alternatively, could write a method to find all unwanted symbols in all lines and 
+            //replace them instead of replacing each line individually
+
         }
     }
 }
